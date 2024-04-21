2cbabeb41215c7569ec0e89931231911a77a5e9e
mecias <mmnc12@gmail.com>
2024-04-20 22:04:13 -0300

    rocketeseat_api-front-end

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import { FiPower } from "react-icons/fi";
+import { useAuth } from "../../hooks/auth";
+import { api } from "../../services/api";
+import { Container, Profile, Logout } from "./styles";
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+
+
+export function Header() {
+  const { signOut, user } = useAuth();
+  const navigation = useNavigate();
+
+  function handleSignOut() {
+    navigation("/");
+    signOut();
+  }
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+  return (
+    <Container>
+      <Profile to="/profile">
+        <img 
+          src={avatarUrl}
+          alt={user.name} 
+        />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>{user.name}</strong>
+        </div>
+      </Profile>
+
+      <Logout onClick={handleSignOut}>
+        <FiPower />
+      </Logout>
+    </Container>
+  )
+}
